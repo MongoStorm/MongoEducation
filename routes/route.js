c475e7cf20cff6bfd809a057be6f8a06581a13b0
simpletrain <zsy498761554@live.com>
2015-08-24 15:10:28 +0800

    add chapter routes & views

--- routes/route.js
+++ routes/route.js
@@ -3,3 +3,7 @@
 exports.setRoutes = function(app) {
   app.use('/', require('./routers/index'));
 };
+
+exports.setRoutes = function(app) {
+  app.use('/user/courses_1/chapter_1', require('./routers/user/courses/chapters/chapter-1'));
+};
