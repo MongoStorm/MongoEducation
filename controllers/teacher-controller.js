4113ae3acf818c9b7d0790b2894bfccec4e23a11
carolineF <1138483832@qq.com>
2015-08-24 23:24:39 +0800

    use the mock data to webpage

--- controllers/teacher-controller.js
+++ controllers/teacher-controller.js
@@ -4,7 +4,7 @@ function TeacherController() {
 }
 
 TeacherController.prototype.index = function(req, res) {
-  res.render('teacher-profile');
+  res.render('teacher-profile',{courses: ['第一个课程的名称','第二个课程的名称','第三个课程的名称']});
 };
 
 module.exports = TeacherController;
