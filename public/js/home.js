4f513b1b367643c661da14216dd8b0da945f5fde
scq000 <scq000@hotmail.com>
2015-08-25 07:30:13 +0800

    add pagination

---
+++ public/js/home.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $('#pagination').twbsPagination({
+    first: '首页',
+    prev: '前一页',
+    next: '下一页',
+    last: '最后一页',
+    totalPages: 35,
+    visiblePages: 7,
+    onPageClick: function (event, page) {
+      //control page content
+    }
+  });
+});
