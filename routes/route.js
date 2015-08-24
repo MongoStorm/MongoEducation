d79f0f9e63bfbcce2bcc377160febf5c0e974dc6
sialvsic <sialvsic@outlook.com>
2015-08-24 17:06:07 +0800

    modified route.js

--- routes/route.js
+++ routes/route.js
@@ -3,5 +3,8 @@
 exports.setRoutes = function(app) {
   app.use('/', require('./routers/index'));
   app.use('/user/courses_1/chapter_1', require('./routers/user/courses/chapters/chapter-1'));
+
   app.use('/user/courses_1', require('./routers/user/courses/course-1'));
+
+  app.use('/login',require('./routers/login/login'));
 };
