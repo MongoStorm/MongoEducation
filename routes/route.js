ad7feda9bea68dc0285558914d530d9cb95874cd
liya <liya_013@live.com>
2015-08-24 20:26:44 +0800

    change url to :course_id

--- routes/route.js
+++ routes/route.js
@@ -4,7 +4,7 @@ exports.setRoutes = function(app) {
   app.use('/', require('./routers/index'));
   app.use('/user/courses_1/chapter_1', require('./routers/user/courses/chapters/chapter-1'));
 
-  app.use('/user/courses_1', require('./routers/user/courses/course-1'));
+  app.use('/user/:course_id', require('./routers/user/courses/course'));
 
   app.use('/login',require('./routers/login/login'));
 };
