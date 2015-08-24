39cab45e194e4673796aa7afe9b8abcc310854e4
liya <liya_013@live.com>
2015-08-24 17:10:36 +0800

    add course relate file

--- routes/route.js
+++ routes/route.js
@@ -3,4 +3,5 @@
 exports.setRoutes = function(app) {
   app.use('/', require('./routers/index'));
   app.use('/user/courses_1/chapter_1', require('./routers/user/courses/chapters/chapter-1'));
+  app.use('/user/courses_1', require('./routers/user/courses/course-1'));
 };
