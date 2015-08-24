d9819841e43b34b45f3c60d3c2b4a0fc7c334114
carolineF <1138483832@qq.com>
2015-08-24 15:36:41 +0800

    render the static jade

--- routes/routers/index.js
+++ routes/routers/index.js
@@ -7,4 +7,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/teacherProfile', function(req, res) {
+  res.render('teacher-profile');
+});
+
 module.exports = router;
