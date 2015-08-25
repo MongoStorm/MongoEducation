4a20e9c2646139b5f4479cd59657f72fbba50280
XiaoLongW <WangXiaoLong2016@outlook.com>
2015-08-25 10:10:57 +0800

    add chapterId

--- public/js/course-add.js
+++ public/js/course-add.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
-  var news = $('#new');
-
-  news.click(function(){
-    $('form').append("<section class='container well panel-primary'> <ul class='list-unstyled list-group'> <li class='list-group-item col-md-12'> <label>第1章节:</label> <input type='text' class='form-control'/> </li> <li class='list-group-item col-md-12'> <label class='col-md-12'>上传文件:</label> <input type='file' value='上传文件' class='col-md-2 form-control'/> <label class='col-md-11'> </label> <input type='button' id='update' value='上传' class='col-md-1'/> </li> <li class='list-group'> <input type='button' id='new' value='新增' class='col-md-6'/> <input type='button' id='delete' value='删除' class='col-md-6'/> </li> </ul> </section>");
+  var addBtn = $('#addBtn:last');
+  var chapterId = 2;
+  addBtn.click(function(){
+    $('form').append("<section class='container well panel-primary'> <ul class='list-unstyled list-group'> <li class='list-group-item col-md-12'> <label id='chapter'>第"+chapterId+"章节:</label> <input type='text' class='form-control'/> </li> <li class='list-group-item col-md-12'> <div class='col-md-12'>上传文件:</div> <input type='file' value='上传文件' class='col-md-2 form-control'/> <div class='col-md-11'> </div> <input type='button' id='update' value='上传' class='col-md-1'/> </li> <li class='list-group'><div class='col-md-5'></div> <input type='button' id='new' value='新增' class='col-md-1'/> <input type='button' id='delete' value='删除' class='col-md-1'/> </li> </ul> </section>");
+    chapterId++;
   });
 });
