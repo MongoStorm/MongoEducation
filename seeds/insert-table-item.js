03f97b03db46aee24b762617df8511bca12800cb
simpletrain <zsy498761554@live.com>
2015-08-25 16:34:31 +0800

    change video require path

--- seeds/insert-table-item.js
+++ seeds/insert-table-item.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var db = require('../models/index');
-var video = require(__dirname + '/../config/video.json');
+var video = require('../config/video.json');
 
 db.Course.create({name: 'bootstrap', description: 'Bootstrap'}).then(function (bootstrap) {
   db.Chapter.bulkCreate([{name: 'first', videoUrl:video.path+'1-1.ogg'}, {name: 'second', videoUrl:video.path+'1-2.ogg'}])
