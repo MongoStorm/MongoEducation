e5fd8471ad901ac3998aa2b09a57777a86bd48c3
sialvsic <sialvsic@outlook.com>
2015-08-25 16:53:59 +0800

    add click method and ajax require

--- public/js/login.js
+++ public/js/login.js
@@ -1,8 +1,43 @@
 $(document).ready(function () {
-  $('#submitbutton').on('click',function(){
+  $('#studentsubmit').on('click',function(){
+    $.ajax({
+      type: "POST",
+      url: "/login",
+      data: {userInput:$("#email").val(), password:$("#studentpassword").val()},
+      dataType: "json",
+      complete: function(data) {
 
+      },
+      statusCode: {
+        404: function() {
+          alert( "page not found" );
+        }
+      }
 
-    $('form').submit();
+    });
   });
+
+
+  $('#teachersubmit').on('click',function(){
+
+    $.ajax({
+      type: "POST",
+      url: "/login",
+      data: {userInput:$("#employeeId").val(), password:$("#teacherpassword").val()},
+      dataType: "json",
+      complete: function(data) {
+
+      },
+
+      statusCode: {
+        404: function() {
+          alert( "page not found" );
+        }
+      }
+
+    });
+  });
+
+
 });
 
