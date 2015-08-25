d7b8b43a52fd49acbfe913c06f464e1d02bcd99e
liya <liya_013@live.com>
2015-08-25 14:08:27 +0800

    add coursedataHelper to get data

--- controllers/course-controller.js
+++ controllers/course-controller.js
@@ -1,10 +1,18 @@
 'use strict';
 
+var CourseDataHelper = require('../helpers/course-data-helper');
+
 function CourseController() {
 
 }
 
 CourseController.prototype.index = function(req,res) {
+
+  var courseDataHelper = new CourseDataHelper();
+  courseDataHelper.getCourseData(function(data){
+    console.log(data);
+  });
+
   res.render('_course');
 };
 
