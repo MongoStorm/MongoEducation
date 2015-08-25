cbeec9d004a9a02b7eeb7056b37a92d4eeae9cf4
carolineF <1138483832@qq.com>
2015-08-25 10:16:14 +0800

    replace bootstrap-pagination with twbs-pagination

--- public/js/teacher-profile.js
+++ public/js/teacher-profile.js
@@ -1,27 +1,13 @@
-$(function() {
-
-  var options = {
-    currentPage:2,
-    totalPages:10,
-    numberOfPages:5,
-    itemTexts: function (type, page, current) {
-      switch (type) {
-        case "first":
-          return "首页";
-        case "prev":
-          return "上一页";
-        case "next":
-          return "下一页";
-        case "last":
-          return "末页";
-        case "page":
-          return  page;
-      }
-    },
-    alignment:"center"
-  };
-
-  $('#page').bootstrapPaginator(options);
-  $('#page>ul').addClass('pagination');
-
+$(function () {
+  $('#pagination').twbsPagination({
+    first: '首页',
+    prev: '前一页',
+    next: '下一页',
+    last: '最后一页',
+    totalPages: 10,
+    visiblePages: 7,
+    onPageClick: function (event, page) {
+      //control page content
+    }
+  });
 });
