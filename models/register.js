70d9120e82c09f16d55ccafbffac60678dfb427c
Evan <EvanLee_@outlook.com>
2015-08-25 16:00:38 +0800

    add register.insert

--- models/register.js
+++ models/register.js
@@ -1,4 +1,11 @@
 'use strict';
 var db = require('./index');
 
+function Register(){
+
+}
+Register.prototype.insert = function(email,password){
+  db.Students.create({email:email,password:password})
+};
+
 
