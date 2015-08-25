a09ff521099b20a7d3432a0a94eadb4c16bbd288
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-08-25 15:58:58 +0800

    add the function to judge is login

---
+++ public/js/header.js
@@ -0,0 +1,22 @@
+$(function () {
+  var id = $.cookie('id');
+  if(id === 'null'){
+    return;
+  }
+
+  var type = $.cookie('type');
+  $('.userInfo').prepend('你好：',id);
+  $('.dropdown-menu').empty().prepend('<li><a id="exit" href="/">注销</a></li>');
+
+  if(type === 'teacher'){
+    $('.dropdown-menu').prepend('<li><a href="/">个人中心</a></li>');
+  }
+  $('.offline').css('display','none');
+  $('.online').css('display','block');
+
+  $('#exit').on('click',function(){
+    $.cookie('id',null);
+    $.cookie('type',null);
+    window.location.href='localhost:3000';
+  });
+});
