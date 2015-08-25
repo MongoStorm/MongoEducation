59f2ee38b9c705f240e0459e701dbb6542d5d698
carolineF <1138483832@qq.com>
2015-08-25 14:40:46 +0800

    add relationship between course and teacher

--- models/course.js
+++ models/course.js
@@ -7,6 +7,7 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         Course.hasMany(models.Chapter,{as: 'Chapter'});
+        Course.belongsTo(models.Teacher,{as: 'Teacher'});
       }
     }
   });
