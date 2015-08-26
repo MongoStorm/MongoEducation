a95575026601a52f28c4cd098e9d7fc9239514ac
scq000 <scq000@hotmail.com>
2015-08-26 12:58:40 +0800

    use course data to render course list

--- controllers/home.js
+++ controllers/home.js
@@ -1,9 +1,19 @@
 'use strict';
 
-function HomeController() {}
+var allCourses = require('../seeds/courses').courses;
 
-HomeController.prototype.index = function (req,res) {
-  res.render('index');
+function HomeController() {
+}
+
+HomeController.prototype.index = function (req, res) {
+  var page = req.query.page || 1;
+  var courseNum = 8;
+
+  var totalPages = Math.ceil(allCourses.length / courseNum);
+
+  var courses = allCourses.slice((page - 1) * courseNum, page * courseNum);
+
+  res.render('index', {courses: courses,totalPages: totalPages});
 };
 
 module.exports = HomeController;
--- public/js/home.js
+++ public/js/home.js
@@ -1,10 +1,13 @@
 $(document).ready(function () {
+
+  var totalPages = $('#pagination').attr('totalPages');
+
   $('#pagination').twbsPagination({
     first: '首页',
     prev: '前一页',
     next: '下一页',
     last: '最后一页',
-    totalPages: 35,
+    totalPages: totalPages,
     visiblePages: 7,
     href: '?page={{number}}',
     onPageClick: function (event, page) {
