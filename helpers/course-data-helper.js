ba2f11eaec33238963c54a28008362a5ec433628
liya <liya_013@live.com>
2015-08-26 11:21:07 +0800

    get course and chapter data from database

--- helpers/course-data-helper.js
+++ helpers/course-data-helper.js
@@ -1,25 +1,37 @@
 'use strict';
+
 var data = require('../config/config.json');
 var db = require('../models/index');
-var Sequelize = require('sequelize');
+
 function CourseDataHelper() {
 
 }
 
 CourseDataHelper.prototype.getCourseData = function(callback) {
 
-  db.Course.findAll({
-    include: [{
-      model: db.Chapter,
-      where: { id :db.Chapter.CourseId }
-    }]
-  })
+  db.Course.findAll({attributes: ['id', 'name','description']})
     .then(function(courses) {
-      //console.log(courses);
-
-      callback(courses);
+      callback(getFormatQuery(courses));
     });
 };
+
+CourseDataHelper.prototype.getChapterData = function(callback) {
+
+  db.Chapter.findAll({attributes: ['CourseId','name','videoUrl']})
+    .then(function(chapters) {
+      callback(getFormatQuery(chapters));
+    });
+};
+
+function getFormatQuery(datas) {
+  var result = [];
+
+  datas.forEach(function(data) {
+    result.push(data.dataValues);
+  });
+
+  return result;
+}
 
 module.exports = CourseDataHelper;
 
