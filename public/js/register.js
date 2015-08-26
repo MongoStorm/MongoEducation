0b558aab3201ad99d2a655c7488cbe97aa996802
Evan <EvanLee_@outlook.com>
2015-08-26 16:42:25 +0800

    add judge logic

--- public/js/register.js
+++ public/js/register.js
@@ -1,1 +1,23 @@
+$(document).ready(function(){
+  $('#sign-up').on('click',function(){
+    if($('#email').val() === ''){
+      $('#email-error').css('display', 'none');
+      $('#email-null').css('display', 'block');
+      return ;
+    }
+    var judge=/^([a-zA-Z0-9]+[_|-|.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|-|.]?)*[a-zA-Z0-9]+.[a-zA-Z]{2,3}$/;
 
+    if(judge.test($('#email').val()) == false){
+      $('#email-null').css('display', 'none');
+      $('#email-error').css('display', 'block');
+      return ;
+    }
+    if($('#password').val() === ''){
+      $('#email-error').css('display', 'none');
+
+      return ;
+    }
+
+    $('form').submit();
+  });
+});
