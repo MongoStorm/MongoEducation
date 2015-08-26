97d02e8261db79b74afe17044775aa172c10c47b
liya <liya_013@live.com>
2015-08-26 11:21:56 +0800

    use course helper

--- controllers/course.js
+++ controllers/course.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var CourseDataHelper = require('../helpers/course-data-helper');
-var Sequelize = require('sequelize');
+
 function CourseController() {
 
 }
@@ -9,10 +9,15 @@ function CourseController() {
 CourseController.prototype.index = function(req,res) {
 
   var courseDataHelper = new CourseDataHelper();
-  var courses = courseDataHelper.getCourseData(function(courses) {
+
+  courseDataHelper.getCourseData(function(courses) {
     console.log(courses);
     res.render('course',courses);
   });
+  courseDataHelper.getChapterData(function(chapters) {
+    console.log(chapters);
+  });
+
 
 };
 
