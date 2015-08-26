70e010afa30c0811c45df10b5529267fbb4f140a
XiaoLongW <WangXiaoLong2016@outlook.com>
2015-08-26 15:49:56 +0800

    insert data to student table

--- controllers/course-add.js
+++ controllers/course-add.js
@@ -4,13 +4,13 @@ var db = require('../models/index');
 
 function CourseAddController() {}
 
-CourseAddController.prototype.new = function(req, res) {
+CourseAddController.prototype.show = function(req, res) {
   res.render('course-add');
 };
 
-CourseAddController.prototype.show = function(req, res) {
-  db.Chapter.insertChapterData(req.chapter_1, 1, req.commit_file_1);
-  res.render('teacher-profile');
+CourseAddController.prototype.new = function(req, res) {
+  db.Course.interCourseData(req.body.course_name, req.body.course_desc);
+  res.render('course-add');
 };
 
 module.exports = CourseAddController;
