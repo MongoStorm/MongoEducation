ea4b764b7e964a230c6711f0597d2bbdc26011ac
simpletrain <zsy498761554@live.com>
2015-08-26 14:25:39 +0800

    format helper

--- helpers/course-helper.js
+++ helpers/course-helper.js
@@ -4,16 +4,16 @@ function CourseHelper() {
 
 }
 
-CourseHelper.prototype.getFormatData = function(Courses,Chapters) {
+CourseHelper.prototype.getFormatData = function (Courses, Chapters) {
   var courses = [];
-  Courses.forEach(function(Course) {
+  Courses.forEach(function (Course) {
     var chapters = [];
-    Chapters.forEach(function(Chapter) {
-      if(Course.id === Chapter.CourseId) {
-        chapters.push({name:Chapter.name, videoUrl:Chapter.videoUrl});
+    Chapters.forEach(function (Chapter) {
+      if (Course.id === Chapter.CourseId) {
+        chapters.push({name: Chapter.name, videoUrl: Chapter.videoUrl});
       }
     });
-    courses.push({name:Course.name, description:Course.description, chapters: chapters});
+    courses.push({name: Course.name, description: Course.description, chapters: chapters});
   })
   return courses;
 };
