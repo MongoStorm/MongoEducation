f51b22ed4dfe0ed57235d9f1229bbe267d9aa6ed
XiaoLongW <WangXiaoLong2016@outlook.com>
2015-08-26 14:30:08 +0800

    modify course-add controller

--- controllers/course-add.js
+++ controllers/course-add.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var db = require('../models/index');
+
 function CourseAddController() {}
 
 CourseAddController.prototype.new = function(req, res) {
@@ -7,8 +9,8 @@ CourseAddController.prototype.new = function(req, res) {
 };
 
 CourseAddController.prototype.show = function(req, res) {
-
-  res.render('teacher-profilr')
+  db.Chapter.insertChapterData(req.chapter_1, 1, req.commit_file_1);
+  res.render('profilr');
 };
 
 module.exports = CourseAddController;
