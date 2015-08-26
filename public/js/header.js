88b5a37848ac859aa4be9fe53f2eefb93a997205
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-08-26 11:27:14 +0800

    format the code

--- public/js/header.js
+++ public/js/header.js
@@ -1,22 +1,22 @@
 $(function () {
   var id = $.cookie('id');
-  if(id === 'null' || id === undefined || id === ''){
+  if (id === 'null' || id === undefined || id === '') {
     return;
   }
 
   var type = $.cookie('type');
-  $('.userInfo').prepend('你好：',id);
+  $('.userInfo').prepend('你好：', id);
   $('.dropdown-menu').empty().prepend('<li><a id="exit" href="/">注销</a></li>');
 
-  if(type === 'teacher'){
+  if (type === 'teacher') {
     $('.dropdown-menu').prepend('<li><a href="/profile">个人中心</a></li>');
   }
-  $('.offline').css('display','none');
-  $('.online').css('display','block');
+  $('.offline').css('display', 'none');
+  $('.online').css('display', 'block');
 
-  $('#exit').on('click',function(){
-    $.cookie('id',null);
-    $.cookie('type',null);
-    window.location.href='/';
+  $('#exit').on('click', function () {
+    $.cookie('id', null);
+    $.cookie('type', null);
+    window.location.href = '/';
   });
 });
