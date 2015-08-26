40f291d2011a382d0e5cda563dd2ba367c4e91e1
Evan <EvanLee_@outlook.com>
2015-08-27 07:14:20 +0800

    add 'use strict' & change the judge->JUDGE

--- public/js/register.js
+++ public/js/register.js
@@ -1,13 +1,17 @@
+'use strict';
+
 $(document).ready(function(){
   $('#sign-up').on('click',function(){
-    if($('#email').val() === ''){
+    var $email = $('#email').val();
+
+    if($email === ''){
       $('#email-error').css('display', 'none');
       $('#email-null').css('display', 'block');
       return ;
     }
-    var judge=/^([a-zA-Z0-9]+[_|-|.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|-|.]?)*[a-zA-Z0-9]+.[a-zA-Z]{2,3}$/;
+    var JUDGE=/^([a-zA-Z0-9]+[_|-|.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|-|.]?)*[a-zA-Z0-9]+.[a-zA-Z]{2,3}$/;
 
-    if(judge.test($('#email').val()) == false){
+    if(JUDGE.test($email) == false){
       $('#email-null').css('display', 'none');
       $('#email-error').css('display', 'block');
       return ;
