543bf948ddc840c9c80e2b12f05d85ff766d705d
Evan <EvanLee_@outlook.com>
2015-08-27 07:50:47 +0800

    add the judgement to password

--- public/js/register.js
+++ public/js/register.js
@@ -1,23 +1,52 @@
 'use strict';
 
 $(document).ready(function(){
-  $('#sign-up').on('click',function(){
-    var $email = $('#email').val();
 
-    if($email === ''){
+  var $email = $('#email');
+
+  $($email).on('blur',function(){
+
+    if($email.val() === ''){
       $('#email-error').css('display', 'none');
       $('#email-null').css('display', 'block');
+
       return ;
     }
+
     var JUDGE=/^([a-zA-Z0-9]+[_|-|.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|-|.]?)*[a-zA-Z0-9]+.[a-zA-Z]{2,3}$/;
 
-    if(JUDGE.test($email) == false){
+    if(JUDGE.test($email.val()) == false){
       $('#email-null').css('display', 'none');
       $('#email-error').css('display', 'block');
+
+      return ;
+    }else{
+      $('#email-null').css('display', 'none');
+      $('#email-error').css('display', 'none');
       return ;
     }
-    if($('#password').val() === ''){
+    
+  });
+
+  $('#sign-up').on('click',function(){
+
+
+
+    var PASSWORD = /^(\w){6,20}$/;
+    var $password = $('#password').val();
+    if(PASSWORD.test($password) === false){
       $('#email-error').css('display', 'none');
+      $('#email-null').css('display', 'none');
+      $('#password-error').css('display','block');
+
+      return ;
+    }
+
+    if($password !== $('#conifrm').val()){
+      $('#email-error').css('display', 'none');
+      $('#email-null').css('display', 'none');
+      $('#password-error').css('display','none');
+      $('#confirm-error').css('display','block');
 
       return ;
     }
