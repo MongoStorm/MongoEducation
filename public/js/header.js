26d8d1f48742c89998bf2cd75995394733168bea
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-08-26 21:43:58 +0800

    fix the code review bug

--- public/js/header.js
+++ public/js/header.js
@@ -1,20 +1,18 @@
 $(function () {
   var id = $.cookie('id');
   var type = $.cookie('type');
-  if (id === undefined || id === '') {
-    return;
-  }
-  if (type === undefined || type === '') {
+  if (!id || !type) {
     return;
   }
 
-  var type = $.cookie('type');
   $('.userInfo').prepend('你好：', id);
-  $('.dropdown-menu').empty().prepend('<li><a id="exit" href="/login/logout">注销</a></li>');
+  var $dropdownMenu = $('.dropdown-menu');
+  $dropdownMenu.empty().prepend('<li><a id="exit" href="/login/logout">注销</a></li>');
 
   if (type === 'teacher') {
-    $('.dropdown-menu').prepend('<li><a href="/profile">个人中心</a></li>');
+    $dropdownMenu.prepend('<li><a href="/profile">个人中心</a></li>');
   }
+
   $('.offline').css('display', 'none');
   $('.online').css('display', 'block');
 });
