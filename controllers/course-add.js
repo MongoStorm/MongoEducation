4921506500c70b48e714e4a434464577c582282e
XiaoLongW <WangXiaoLong2016@outlook.com>
2015-08-26 10:52:36 +0800

    add post router

--- controllers/course-add.js
+++ controllers/course-add.js
@@ -2,8 +2,12 @@
 
 function CourseAddController() {}
 
-CourseAddController.prototype.new = function (req,res) {
+CourseAddController.prototype.new = function(req, res) {
   res.render('course-add');
 };
+
+CourseAddController.prototype.show = function(req, res) {
+  res.render('teacher-profilr')
+}
 
 module.exports = CourseAddController;
