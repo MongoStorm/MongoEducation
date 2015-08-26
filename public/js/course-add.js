2abb80aa5b37bd1176ed6af6e44d1e96ad431de0
XiaoLongW <WangXiaoLong2016@outlook.com>
2015-08-26 21:53:21 +0800

    change the js

--- public/js/course-add.js
+++ public/js/course-add.js
@@ -1,7 +1,26 @@
-var chapterId = 2;
+$(function () {
+  $('input[name="addBtn"]').on('click',function(){
+    addChapter();
+  })
+});
+
 function addChapter(){
-  $('#submit').before('<section class="container well panel-primary"> <ul class="list-unstyled list-group"> <li class="list-group-item col-md-12"> <label id="chapter">第'+chapterId+'章节:</label> <input type="text" name='+'"'+'chapter_'+chapterId+'"'+' class="form-control" placeholder="请输入章节名" /> </li> <li class="list-group-item col-md-12"> <div class="col-md-12">上传文件:</div> <input type="file" value="上传文件" name="'+'commit_file_'+chapterId+'" class="col-md-2 form-control"/> <div class="col-md-5"> </div> <input type="button" id="update" value="上传" class="col-md-1 btn btn-primary text-right sectionbk"/> </li> <li class="list-group"><div class="col-md-9"></div> <input type="button" id="new"  onclick="addChapter()" value="新增" class="col-md-1 btn btn-primary text-right sectionbk"/> <input type="button" id="delete" value="删除" onclick="delChapter()" class="col-md-1 btn btn-primary text-right sectionbk"/> </li> </ul> </section>');
-  chapterId++;
+  var chapterId = parseInt($('label[name="chapterId"]:last').text()[1]) + 1;
+  $('#submit').before('<section class="container well panel-primary"> ' +
+                      '<ul class="list-unstyled list-group"> ' +
+                      '<li class="list-group-item col-md-12"> ' +
+                      '<label name="chapterId">第'+chapterId+'章节:</label> ' +
+                      '<input type="text" name='+'"'+'chapter_'+chapterId+'"'+' class="form-control" placeholder="请输入章节名" />' +
+                      '</li>' +
+                      '<li class="list-group-item col-md-12"> <div class="col-md-12">上传文件:</div>' +
+                      '<input type="file" value="上传文件" name="'+'commit_file_'+chapterId+'" class="col-md-2 form-control"/>' +
+                      '<div class="col-md-5"> </div> ' +
+                      '<input type="button" id="update" value="上传" class="col-md-1 btn btn-primary text-right sectionbk"/> ' +
+                      '</li> ' +
+                      '<li class="list-group"><div class="col-md-9"></div> ' +
+                      '<input type="button" id="new"  onclick="addChapter()" value="新增" class="col-md-1 btn btn-primary text-right sectionbk"/>' +
+                      '<input type="button" id="delete" value="删除" onclick="delChapter()" class="col-md-1 btn btn-primary text-right sectionbk"/> ' +
+                      '</li> </ul> </section>');
 }
 
 function delChapter(){
