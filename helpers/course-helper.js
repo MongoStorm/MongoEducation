82e99ef578700a707c9e636a56ca0a0d9881786a
liya <liya_013@live.com>
2015-08-26 16:36:32 +0800

    add chapter id

--- helpers/course-helper.js
+++ helpers/course-helper.js
@@ -8,13 +8,15 @@ CourseHelper.prototype.getFormatData = function (Courses, Chapters) {
   var courses = [];
   Courses.forEach(function (Course) {
     var chapters = [];
-    Chapters.forEach(function (Chapter) {
-      if (Course.id === Chapter.CourseId) {
-        chapters.push({name: Chapter.name, videoUrl: Chapter.videoUrl});
+
+    Chapters.forEach(function(Chapter) {
+      if(Course.id === Chapter.CourseId) {
+        chapters.push({id: Chapter.id, name: Chapter.name, videoUrl: Chapter.videoUrl});
       }
     });
-    courses.push({name: Course.name, description: Course.description, chapters: chapters});
-  })
+    courses.push({id: Course.id, name: Course.name, description: Course.description, chapters: chapters});
+  });
+
   return courses;
 };
 module.exports = CourseHelper;
