c9b99420ffce9d19a98505a8100c54943297e4fe
Evan <EvanLee_@outlook.com>
2015-08-27 11:15:24 +0800

    change the judge()

--- models/student.js
+++ models/student.js
@@ -6,15 +6,25 @@ module.exports = function(sequelize, DataTypes) {
     password: DataTypes.STRING
   }, {
     classMethods: {
-      verify:function(email,password,callback){
+      verify: function (email, password, callback) {
         this.findAll({
           where: {
             email: email,
             password: password
           }
-        }).then(function(data){
+        }).then(function (data) {
           callback(data.length > 0);
         })
+      },
+      judge: function (email,callback) {
+        this.findAll({
+          where: {
+            email: email
+          }
+        }).then(function (result) {
+          callback(result.length > 0);
+        });
+
       }
     }
   });
