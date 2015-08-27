f0a719f65f6daba0eee4b658b377dec7840c2482
liya <liya_013@live.com>
2015-08-27 17:22:22 +0800

    change CourseId to courseId

---
+++ migrations/20150827153357-rename-chapter.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.renameColumn('Chapters', 'CourseId', 'courseId');
+
+  },
+
+  down: function (queryInterface, Sequelize) {
+
+  }
+};
