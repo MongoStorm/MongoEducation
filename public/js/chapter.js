fd60df7b5f9b3bcbce02c00e262aee7800e51a5e
simpletrain <zsy498761554@live.com>
2015-08-27 16:22:19 +0800

    add "last & next chapter" function in chapter view

--- public/js/chapter.js
+++ public/js/chapter.js
@@ -4,4 +4,12 @@ $(function () {
   $('#return').on('click', function() {
     location.href = '/courses/' + $(this).val();
   });
+
+  $('#last').on('click', function() {
+    location.href = '/courses/' + $('#return').val() + '/chapters/' + $(this).val();
+  });
+
+  $('#next').on('click', function() {
+    location.href = '/courses/' + $('#return').val() + '/chapters/' + $(this).val();
+  });
 });
