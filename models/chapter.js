6dedd5021e860c5c184e73dcf5ff81bc49c2abc9
liya <liya_013@live.com>
2015-08-28 00:46:21 +0800

    add courseId to model chapter

--- models/chapter.js
+++ models/chapter.js
@@ -3,11 +3,12 @@
 module.exports = function (sequelize, DataTypes) {
   var Chapter = sequelize.define('Chapter', {
     name: DataTypes.STRING,
-    videoUrl: DataTypes.STRING
+    videoUrl: DataTypes.STRING,
+    courseId: DataTypes.INTEGER
   }, {
     classMethods: {
       associate: function (models) {
-        Chapter.belongsTo(models.Course, {as: 'Course', foreignKey: 'courseId'});
+        Chapter.belongsTo(models.Course, {foreignKey: {name:'id'}});
       },
       findChaptersData: function (callback) {
         this.findAll({attributes: ['id', 'courseId', 'name', 'videoUrl']})
