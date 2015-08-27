0abef02f8e2d6da0cf53db81d5d8b37918ccb6ea
scq000 <scq000@hotmail.com>
2015-08-27 15:42:12 +0800

    use ajax to optimize search & page

--- public/js/home.js
+++ public/js/home.js
@@ -2,22 +2,44 @@
 
 $(function () {
 
-  var totalPages = $('#pagination').attr('totalPages');
-  var query = $('form input').val();
-  if (query !== '') {
-    var href = '?page={{number}}&query=' + query;
-  } else {
-    var href = '?page={{number}}';
-  }
+  init();
 
+  $('#search').click(function () {
+    queryAndPage($('#query').val());
+  });
+
+
+});
+
+
+function queryAndPage(query) {
+  $.get('/page-content', {page: 1, query: query}, function (result) {
+    $('#page-content').html(result);
+    initPagination(1, $('#pagination').attr('totalPages'));
+  });
+}
+
+function init() {
+  queryAndPage('');
+}
+
+function initPagination(currentPage,totalPages) {
+  $('#pagination').remove();
+  $('.text-center').html('<div id="pagination" totalPages='+totalPages +'></div>');
   $('#pagination').twbsPagination({
+    startPage: currentPage,
     first: '首页',
     prev: '前一页',
     next: '下一页',
     last: '最后一页',
     totalPages: totalPages,
     visiblePages: 7,
-    href: href
+    onPageClick: function (event, page) {
+      $.get('/page-content',{page:page, query: $('#query').val()},function (result) {
+        $('#page-content').html(result);
+        initPagination(currentPage+1,totalPages);
+      });
+    }
   });
 
-});
+}
