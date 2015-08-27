10ea5a6051c9939e196a45710f82059e5400a0a3
Evan <EvanLee_@outlook.com>
2015-08-27 13:43:38 +0800

    add a add() in student

--- models/student.js
+++ models/student.js
@@ -1,4 +1,5 @@
 'use strict';
+var bcrypt = require('../node_modules/bcrypt-nodejs/bCrypt');
 
 module.exports = function(sequelize, DataTypes) {
   var Student = sequelize.define('Student', {
@@ -24,7 +25,13 @@ module.exports = function(sequelize, DataTypes) {
         }).then(function (result) {
           callback(result.length > 0);
         });
-
+      },
+      add:function(email,password){
+        var salt = bcrypt.genSaltSync(8);
+        console.log(salt);
+        var hash = bcrypt.hashSync(password,salt,null);
+        console.log(hash);
+        this.create({email: email, password: hash});
       }
     }
   });
