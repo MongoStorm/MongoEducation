0d5234e08ab6d12c40578a1644763250b4bee9d4
XiaoLongW <WangXiaoLong2016@outlook.com>
2015-08-27 23:42:18 +0800

    change router

--- routes/routers/management.js
+++ routes/routers/management.js
@@ -10,7 +10,7 @@ router.get('/courses', teacherController.show);
 
 router.post('/courses', teacherController.delete);
 
-router.get('/courses/create',teacherController.create );
-router.post('/courses/new',teacherController.new);
+router.get('/courses/new',teacherController.new );
+router.post('/courses/create',teacherController.create);
 
 module.exports = router;
