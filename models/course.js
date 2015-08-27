a2f0713780ec383c06221b22bbfc94203963abd3
scq000 <scq000@hotmail.com>
2015-08-27 22:16:23 +0800

    insert test data

--- models/course.js
+++ models/course.js
@@ -3,7 +3,8 @@
 module.exports = function (sequelize, DataTypes) {
   var Course = sequelize.define('Course', {
     name: DataTypes.STRING,
-    description: DataTypes.STRING
+    description: DataTypes.STRING,
+    categoryId: DataTypes.INTEGER
   }, {
     classMethods: {
       associate: function (models) {
