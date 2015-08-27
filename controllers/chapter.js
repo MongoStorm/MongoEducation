b955b887763f26bf9d65593c786338ee41c2b846
simpletrain <zsy498761554@live.com>
2015-08-27 14:38:31 +0800

    add 4 method in chapter controller

--- controllers/chapter.js
+++ controllers/chapter.js
@@ -1,12 +1,49 @@
 'use strict';
 
+var db = require('../models/index');
+var Chapter = db.Chapter;
+
 function ChapterController() {
 
 }
 
 ChapterController.prototype.show = function(req,res) {
+  Chapter.findAll().then(function(chaptersInfo){
+    var thisCourseId = parseInt(req.params.course_id);
+    var thisChapterId = parseInt(req.params.chapter_id);
 
-  res.render('chapter');
+    var thisChapters = chaptersInfo.filter(function(chapter){
+      return chapter.dataValues.courseId === thisCourseId
+    });
+
+    var thisVideoUrl = '';
+    thisChapters.forEach(function(chapter){
+        if(chapter.dataValues.id === thisChapterId){
+          thisVideoUrl = chapter.dataValues.videoUrl;
+        }
+      });
+
+    var lastChapterId;
+    thisChapters.forEach(function(chapter){
+      if(chapter.dataValues.id === thisChapterId - 1){
+        lastChapterId = chapter.dataValues.id;
+      }
+    });
+
+    var nextChapterId;
+    thisChapters.forEach(function(chapter){
+      if(chapter.dataValues.id === thisChapterId + 1){
+        nextChapterId = chapter.dataValues.id;
+      }
+    });
+
+    res.render('chapter',{
+      thisCourseId : thisCourseId,
+      thisVideoUrl : thisVideoUrl,
+      lastChapterId : lastChapterId,
+      nextChapterId : nextChapterId
+    });
+  });
 };
 
 module.exports = ChapterController;
