a393e509ea6b193e1eb7e2e7cf863787dd0a96af
liya <liya_013@live.com>
2015-08-28 00:45:46 +0800

    through association to get view data

--- controllers/course.js
+++ controllers/course.js
@@ -2,21 +2,36 @@
 
 var db = require('../models/index');
 var Sequelize = require('sequelize');
+var Course = db.Course;
+var Chapter = db.Chapter;
 function CourseController() {
 
 }
 
 CourseController.prototype.index = function(req,res) {
 
-  db.Course.findAll({
-        include: [{
-          model: db.Chapter,
-          where: {courseId: Sequelize.col('db.Course.id')}
-      }]
+  Course.findAll({
+    include: [{
+      model: Chapter,
+      where: {courseId: Sequelize.col('Course.id')}
+    }]
   }).then(function (datas) {
-    res.render('course',datas);
-  });
+    var courses = [];
+    datas.forEach(function(data) {
 
+      var course = data.dataValues;
+      var singleCourse = {id:course.id, name:course.name, description:course.description};
+      var chapters = [];
+      course.Chapters.forEach(function(chapter) {
+        chapters.push(chapter.dataValues);
+      });
+      singleCourse.chapters = chapters;
+      courses.push(singleCourse);
+    });
+    console.log(courses[0]);
+    var id = req.params.course_id;
+    res.render('course/show',{course:courses[id-1]});
+  });
 };
 
 module.exports = CourseController;
