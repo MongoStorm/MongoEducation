42e9c5e77367ff26cec1d506b9008620ba53b71e
simpletrain <zsy498761554@live.com>
2015-08-28 09:27:47 +0800

    rename chapter controller var

--- controllers/chapter.js
+++ controllers/chapter.js
@@ -9,38 +9,38 @@ function ChapterController() {
 
 ChapterController.prototype.show = function(req,res) {
   Chapter.findAll().then(function(chaptersInfo){
-    var thisCourseId = parseInt(req.params.course_id);
-    var thisChapterId = parseInt(req.params.chapter_id);
+    var currentCourseId = parseInt(req.params.course_id);
+    var currentChapterId = parseInt(req.params.chapter_id);
 
-    var thisChapters = chaptersInfo.filter(function(chapter){
-      return chapter.dataValues.CourseId === thisCourseId
+    var currentChapters = chaptersInfo.filter(function(chapter){
+      return chapter.dataValues.CourseId === currentCourseId
     });
 
-    var thisVideoUrl = '';
-    thisChapters.forEach(function(chapter){
-        if(chapter.dataValues.id === thisChapterId){
-          thisVideoUrl = chapter.dataValues.videoUrl;
+    var currentVideoUrl = '';
+    currentChapters.forEach(function(chapter){
+        if(chapter.dataValues.id === currentChapterId){
+          currentVideoUrl = chapter.dataValues.videoUrl;
         }
       });
 
-    var lastChapterId;
-    thisChapters.forEach(function(chapter){
-      if(chapter.dataValues.id === thisChapterId - 1){
-        lastChapterId = chapter.dataValues.id;
+    var previousChapterId;
+    currentChapters.forEach(function(chapter){
+      if(chapter.dataValues.id === currentChapterId - 1){
+        previousChapterId = chapter.dataValues.id;
       }
     });
 
     var nextChapterId;
-    thisChapters.forEach(function(chapter){
-      if(chapter.dataValues.id === thisChapterId + 1){
+    currentChapters.forEach(function(chapter){
+      if(chapter.dataValues.id === currentChapterId + 1){
         nextChapterId = chapter.dataValues.id;
       }
     });
 
     res.render('chapter/chapter',{
-      thisCourseId : thisCourseId,
-      thisVideoUrl : thisVideoUrl,
-      lastChapterId : lastChapterId,
+      currentCourseId : currentCourseId,
+      currentVideoUrl : currentVideoUrl,
+      previousChapterId : previousChapterId,
       nextChapterId : nextChapterId
     });
   });
