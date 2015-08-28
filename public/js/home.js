57cfe370cff85ea5b4fab175ea5396c63f644119
scq000 <scq000@hotmail.com>
2015-08-28 13:12:29 +0800

    beautify home page

--- public/js/home.js
+++ public/js/home.js
@@ -4,14 +4,14 @@ $(function () {
 
   init();
 
-  $('#search').click(function () {
-    queryAndPage($('#query').val());
+  $('#search').keypress(function (event) {
+    if (event.keyCode === 13) {
+      queryAndPage($(this).val());
+    }
   });
 
-  $('#query').keydown(function (event) {
-    if(event.keyCode==13){
-      queryAndPage($('#query').val());
-    }
+  $('#search').next('button').on('click', function () {
+    queryAndPage($('#search').val());
   });
 
   $('.category').on('click', 'a', function () {
@@ -19,51 +19,52 @@ $(function () {
     classify($(this).attr('categoryId'), level);
   });
 
+  function queryAndPage(query) {
+    $.get('/page-content', {page: 1, query: query}, function (result) {
+      $('#page-content').html(result);
+      initPagination(1, $('#pagination').attr('totalPages'));
+    });
+  }
+
+  function init() {
+    queryAndPage('');
+    classify('', 0);
+  }
+
+  function classify(id, level) {
+    $.get('/category', {id: id, level: level}, function (result) {
+      $('.category ul:gt(' + (level - 1) + ')').remove();
+      $(result).appendTo('.category');
+    });
+
+    $.get('/page-content', {page: 1, query: '', categoryId: id}, function (result) {
+      $('#page-content').html(result);
+      initPagination(1, $('#pagination').attr('totalPages'));
+    });
+  }
+
+  function initPagination(currentPage, totalPages) {
+    $('#pagination').remove();
+    $('.page-foot').html('<div id="pagination" totalPages=' + totalPages + '></div>');
+    $('#pagination').twbsPagination({
+      startPage: currentPage,
+      first: '第一页',
+      prev: '前一页',
+      next: '下一页',
+      last: '最后一页',
+      totalPages: totalPages,
+      visiblePages: 7,
+      onPageClick: function (event, page) {
+        $.get('/page-content', {page: page, query: $('#search').val()}, function (result) {
+          $('#page-content').html(result);
+          initPagination(page, totalPages);
+        });
+      }
+    });
+
+  }
 
 });
 
 
-function queryAndPage(query) {
-  $.get('/page-content', {page: 1, query: query}, function (result) {
-    $('#page-content').html(result);
-    initPagination(1, $('#pagination').attr('totalPages'));
-  });
-}
 
-function init() {
-  queryAndPage('');
-  classify('',0);
-}
-
-function classify(id, level) {
-  $.get('/category', {id: id, level: level}, function (result) {
-    $('.category ul:gt(' + (level - 1) + ')').remove();
-    $(result).appendTo('.category');
-  });
-
-  $.get('/page-content', {page: 1, query:'', categoryId: id}, function (result) {
-    $('#page-content').html(result);
-    initPagination(1, $('#pagination').attr('totalPages'));
-  });
-}
-
-function initPagination(currentPage, totalPages) {
-  $('#pagination').remove();
-  $('.text-center').html('<div id="pagination" totalPages=' + totalPages + '></div>');
-  $('#pagination').twbsPagination({
-    startPage: currentPage,
-    first: '第一页',
-    prev: '前一页',
-    next: '下一页',
-    last: '最后一页',
-    totalPages: totalPages,
-    visiblePages: 7,
-    onPageClick: function (event, page) {
-      $.get('/page-content', {page: page, query: $('#query').val()}, function (result) {
-        $('#page-content').html(result);
-        initPagination(page, totalPages);
-      });
-    }
-  });
-
-}
