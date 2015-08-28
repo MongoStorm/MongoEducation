6a29ab59bf4008d37b19076eb703c61450f1c0dd
scq000 <scq000@hotmail.com>
2015-08-28 10:54:37 +0800

    optimize course data

--- seeds/insert-course-items.js
+++ seeds/insert-course-items.js
@@ -8,53 +8,29 @@ var Student = require('../models/index').Student;
 var Teacher = require('../models/index').Teacher;
 
 var courses = [
-  {"name": "Math1", "description": "This is an English course","categoryId": 1},
-  {"name": "Math2", "description": "This is an English course","categoryId": 2},
-  {"name": "Math3", "description": "This is an English course","categoryId": 3},
-  {"name": "Math4", "description": "This is an English course","categoryId": 11},
-  {"name": "Math5", "description": "This is an English course","categoryId": 11},
-  {"name": "Math6", "description": "This is an English course","categoryId": 1},
-  {"name": "Math7", "description": "This is an English course","categoryId": 2},
-  {"name": "Math8", "description": "This is an English course","categoryId": 3},
-  {"name": "Math9", "description": "This is an English course","categoryId": 12},
-  {"name": "Math10", "description": "This is an English course","categoryId": 13},
-  {"name": "Math11", "description": "This is an English course","categoryId": 13 },
-  {"name": "Math12", "description": "This is an English course","categoryId": 3},
-  {"name": "Math13", "description": "This is an English course","categoryId": 21},
-  {"name": "Math14", "description": "This is an English course","categoryId":11 },
-  {"name": "Math15", "description": "This is an English course","categoryId": 13},
-  {"name": "Math16", "description": "This is an English course","categoryId": 13 },
-  {"name": "Math17", "description": "This is an English course","categoryId": 11},
-  {"name": "Math18", "description": "This is an English course","categoryId": 11},
-  {"name": "Math19", "description": "This is an English course","categoryId": 11},
-  {"name": "Math20", "description": "This is an English course","categoryId": 11},
-  {"name": "Math21", "description": "This is an English course","categoryId": 12},
-  {"name": "Math22", "description": "This is an English course","categoryId": 12},
-  {"name": "Math23", "description": "This is an English course","categoryId": 21},
-  {"name": "Math24", "description": "This is an English course","categoryId": 21},
-  {"name": "Math25", "description": "This is an English course","categoryId": 22},
-  {"name": "Math26", "description": "This is an English course","categoryId": 13},
-  {"name": "Math27", "description": "This is an English course","categoryId": 11},
-  {"name": "Math28", "description": "This is an English course","categoryId": 12},
-  {"name": "Math29", "description": "This is an English course","categoryId": 12},
-  {"name": "Math30", "description": "This is an English course","categoryId": 122},
-  {"name": "Math31", "description": "This is an English course","categoryId": 21},
-  {"name": "Math32", "description": "This is an English course","categoryId": 22},
-  {"name": "Math33", "description": "This is an English course","categoryId": 11},
-  {"name": "Math34", "description": "This is an English course","categoryId": 21},
-  {"name": "Math35", "description": "This is an English course","categoryId": 13},
-  {"name": "Math36", "description": "This is an English course","categoryId": 121},
-  {"name": "Math37", "description": "This is an English course","categoryId": 122},
-  {"name": "Math38", "description": "This is an English course","categoryId": 131},
-  {"name": "Math39", "description": "This is an English course","categoryId": 13}
+  {"name": "Advance French", "description": "This is an Advanced French course","categoryId": 11},
+  {"name": "Basic French", "description": "This is an Basic French course","categoryId": 11},
+  {"name": "Middle French", "description": "This is an Middle French course","categoryId": 11},
+  {"name": "English Writing1", "description": "This is an English Writing course","categoryId": 122},
+  {"name": "English Writing2", "description": "This is an English Writing course","categoryId": 122},
+  {"name": "English Writing3", "description": "This is an English Writing course","categoryId": 122},
+  {"name": "Japanese Writing1", "description": "This is an Japanese course","categoryId": 131},
+  {"name": "Japanese Writing2", "description": "This is an Japanese course","categoryId": 131},
+  {"name": "Japanese Writing3", "description": "This is an Japanese course","categoryId": 131},
+  {"name": "Android Basic", "description": "This is an Android Basic course","categoryId": 22},
+  {"name": "Android Middle", "description": "This is an Android Middle course","categoryId": 22},
+  {"name": "Android Advanced", "description": "This is an Android Advanced course","categoryId": 22},
+  {"name": "Web Basic", "description": "This is an Web Basic course","categoryId": 21},
+  {"name": "Web Middle", "description": "This is an Web Middle course","categoryId": 21},
+  {"name": "Web Advanced", "description": "This is an Web Advanced course","categoryId": 21}
 
 ];
 
-var categories = [{id: 1, name:'1'},{id:2 ,name:'2'},
-  {id:3, name:'3'},{id:11,name:'11',parentId:1},
-  {id:12, name:'12',parentId:1},{id:13,name:'13',parentId:1},
-  {id:21, name:'21',parentId:2},{id:22,name:'22',parentId:2},
-  {id:122, name:'122',parentId:12},{id:131,name:'131',parentId:13}
+var categories = [{id: 1, name:'Language'},{id:2 ,name:'Computer'},
+  {id:3, name:'Physics'},{id:11,name:'French',parentId:1},
+  {id:12, name:'English',parentId:1},{id:13,name:'Japanese',parentId:1},
+  {id:21, name:'Web',parentId:2},{id:22,name:'Android',parentId:2},
+  {id:122, name:'English Writing',parentId:12},{id:131,name:'Japenese Writing',parentId:13}
 ];
 
 Category.bulkCreate(categories).then(function (data) {
